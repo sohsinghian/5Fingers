2ba8f73604479b344165cdca58b3833ce696d52a
sohsinghian <sohsinghian@gmail.com>
2022-04-30 21:23:26 +0800

    created update and delete routes for users

--- backend/router/users.js
+++ backend/router/users.js
@@ -24,16 +24,15 @@ usersRouter.post("/login", async (req, res) => {
     const token = jwt.sign({ email: email }, process.env.SECRET, {
       expiresIn: "300s",
     });
-    // const decodedToken = jwt.verify(token, process.env.SECRET);
     res.json({ status: "ok", message: "user logged in", accessToken: token });
   } else {
     res.status(401).json(usernameOrPasswordError);
   }
 });
 
-usersRouter.get("/logout", (req, res) => {
-  res.json({ status: "ok", message: "logged out" });
-});
+// usersRouter.get("/logout", (req, res) => {
+//   res.json({ status: "ok", message: "logged out" });
+// });
 
 usersRouter.post("/create", async (req, res) => {
   try {
@@ -56,6 +55,59 @@ usersRouter.post("/create", async (req, res) => {
     res.json({ status: "ok", message: "user created" });
   } catch (error) {
     res.status(401).json(usernameOrPasswordError);
+  }
+});
+
+usersRouter.put("/edit", async (req, res) => {
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, 12);
+    const token = req.body.token;
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+
+    const updatedUser = await pool.query(
+      `UPDATE users
+      SET name=$1, gender=$2, dateOfBirth=$3, contact=$4, address=$5, postalCode=$6, email=$7, password=$8
+      WHERE email=$9`,
+      [
+        req.body.name,
+        req.body.gender,
+        req.body.dateOfBirth,
+        req.body.contactNum,
+        req.body.unitNumber,
+        req.body.postalCode,
+        req.body.newEmail,
+        req.body.password,
+        req.body.currentEmail,
+      ]
+    );
+    if (updatedUser.rowCount === 1) {
+      res.json({ status: "ok", message: "user updated" });
+    } else {
+      res.json({ status: "error", message: "problems with updating user" });
+    }
+  } catch (error) {
+    res.json({ status: "error", message: "connection error" });
+  }
+});
+
+usersRouter.delete("/remove", async (req, res) => {
+  try {
+    const { email, password, token } = req.body;
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+
+    if (decodedToken.email === email) {
+      const deletedUser = await pool.query(
+        "DELETE FROM users WHERE email=$1 AND password=$2",
+        [email, password]
+      );
+      if (deletedUser.rowCount === 1) {
+        res.json({ status: "ok", message: "user deleted" });
+      } else {
+        res.json({ status: "error", message: "problems with deleting user" });
+      }
+    }
+  } catch {
+    res.json({ status: "error", message: "connection error" });
   }
 });
 
